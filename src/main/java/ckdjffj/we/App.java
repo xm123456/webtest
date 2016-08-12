8da1fbdf69bf4cc900b0acf725ccf840eed8505c
xm123456 <1139745349@qq.com>
2016-08-12 21:18:29 +0800

    first commit

---
+++ src/main/java/ckdjffj/we/App.java
@@ -0,0 +1,13 @@
+package ckdjffj.we;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
